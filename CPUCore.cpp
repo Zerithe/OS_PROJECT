3c5784924d0080ddb72db5604eb6ce39dc11e29a
kenwin728 <kenwin728@gmail.com>
2024-11-04 21:54:52 +0800

    reduced scheduler-test's process creation speed and increased the speed of executing process commands

--- CPUCore.cpp
+++ CPUCore.cpp
@@ -28,7 +28,7 @@ void CPUCore::runCPU()
 			}
 			this->cpuCycle++;
 		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(200));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 }
 
--- ConsoleManager.cpp
+++ ConsoleManager.cpp
@@ -192,7 +192,7 @@ void ConsoleManager::runSchedulerTest()
 			processNameCounter++;
 		}
 		cpuCycle++;
-		std::this_thread::sleep_for(std::chrono::milliseconds(200));
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	}
 }
 
